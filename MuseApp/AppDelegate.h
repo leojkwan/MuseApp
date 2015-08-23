64e70c1cfcf43ddd858334e5c8a300b6c4368d6b
leokwanbt14 <leokwanbt14@gmail.com>
2015-08-23 11:30:32 -0400

    first commit

---
+++ MuseApp/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  MuseApp
+//
+//  Created by Leo Kwan on 8/23/15.
+//  Copyright (c) 2015 Leo Kwan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ MuseApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MuseApp
+//
+//  Created by Leo Kwan on 8/23/15.
+//  Copyright (c) 2015 Leo Kwan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
