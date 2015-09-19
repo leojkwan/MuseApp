21913277599df5a4a49969bdd3e9f285220378f4
leokwanbt14 <leokwanbt14@gmail.com>
2015-09-19 00:34:09 -0400

    added paging vc

---
+++ MuseApp/MUSHomeViewController.m
@@ -0,0 +1,13 @@
+//
+//  MUSHomeViewController.m
+//  MuseApp
+//
+//  Created by Leo Kwan on 9/18/15.
+//  Copyright © 2015 Leo Kwan. All rights reserved.
+//
+
+#import "MUSHomeViewController.h"
+
+@implementation MUSHomeViewController
+
+@end
