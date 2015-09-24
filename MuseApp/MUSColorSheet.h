13c1cedd27f296f88e0f1e5295842558e94d8ae5
leokwanbt14 <leokwanbt14@gmail.com>
2015-09-24 14:13:59 -0400

    added color sheet singleton

--- MuseApp/MUSColorSheet.h
+++ MuseApp/MUSColorSheet.h
@@ -13,39 +13,44 @@
 
 + (MUSColorSheet *)sharedInstance;
 
-@property (strong, nonatomic) UIColor *segmentBar;
-
-
-// text
-
-
-
-
 // color light background
-@property (strong, nonatomic) UIColor *searchBarBackground;
-@property (strong, nonatomic) UIColor *closeButton;
+
 @property (strong, nonatomic) UIColor *artistLabel;
-
-
+@property (strong, nonatomic) UIColor *segmentBar;
 
 // color light background
 @property (strong, nonatomic) UIColor *segmentNavBackground;
+@property (strong, nonatomic) UIColor *searchBarBackground;
 
 
 
-// color dom
-@property (strong, nonatomic) UIColor *addEntryHome;
+@property (strong, nonatomic) UIColor *musicPlayerStyle;
+
+// color dark
+@property (strong, nonatomic) UIColor *toolbarIcon;
+@property (strong, nonatomic) UIColor *musicPlayIcon;
+@property (strong, nonatomic) UIColor *backButtonText;
+@property (strong, nonatomic) UIColor *nowPlayingText;
+
+// color dark2
+@property (strong, nonatomic) UIColor *sectionText;
+@property (strong, nonatomic) UIColor *sectionbackGround;
 
 //system colors
 @property (strong, nonatomic) UIColor *systemColors;
 
 // entry tool icons
-@property (strong, nonatomic) UIColor *toolbarIconTint;
-
-
+@property (strong, nonatomic) UIColor *keyboardIconTint;
+@property (strong, nonatomic) UIColor *keyboardBackground;
+@property (strong, nonatomic) UIColor *toolbarBackGround;
 @property (strong, nonatomic) UIColor *iconTint;
-//@property (strong, nonatomic) UIColor *segmentBackground;
-//@property (strong, nonatomic) UIColor *segmentBackground;
+
+
+
+// text editor
+@property (strong, nonatomic) UIColor *headerColor;
+@property (strong, nonatomic) UIColor *textColor;
+
 
 
 
