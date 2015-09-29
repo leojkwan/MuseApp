6506c08aea0ff8abcc1f2edb7f63196399e2b111
leokwanbt14 <leokwanbt14@gmail.com>
2015-09-29 11:31:53 -0400

    files deleted

--- Pods/Target Support Files/FCVerticalMenu/FCVerticalMenu-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_FCVerticalMenu : NSObject
-@end
-@implementation PodsDummy_FCVerticalMenu
-@end
--- Pods/Target Support Files/JTHamburgerButton/JTHamburgerButton-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_JTHamburgerButton : NSObject
-@end
-@implementation PodsDummy_JTHamburgerButton
-@end
